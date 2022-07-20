279ce4aa21e52ee349741249208853d5702d5f00
Pigot <pigot715@gmail.com>
2022-07-20 18:50:20 +0300

    Updat of the UI + JS added

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-import ''
+class Calculator {
+    constructor(previousOperandTextElement,currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = underfined
+    }
 
-//Text 
+    delete() {}
 
+    appendNumber(number){}
 
+    //Θα συμβαίνει όταν ο χρήστης πατάει κάποιο operation
+    chooseOperation() {
+}
 
-//Also text
+    compute() {
+
+    }
+
+updateDisplay() {
+
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationsButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data--current-operand]')
